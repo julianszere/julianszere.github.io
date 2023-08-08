a90fbf544505abd06d18bfc84e72a3050362f4b3
Julián Szereszewski <46913397+JulianSzere@users.noreply.github.com>
2023-08-08 20:30:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,98 @@
+class Blog {
+    constructor() {
+        this.imagePostsContainer = document.getElementById('imagePosts');
+        this.textPostsContainer = document.getElementById('textPosts');
+        this.imageTabButton = document.getElementById('imageTab');
+        this.textTabButton = document.getElementById('textTab');
+    }
+
+    init() {
+        this.fetchAndDisplayPosts('texts_data.json');
+        this.fetchAndDisplayPosts('images_data.json');
+
+        this.imageTabButton.addEventListener('click', () => {
+            this.imagePostsContainer.style.display = 'block';
+            this.textPostsContainer.style.display = 'none';
+            this.activateTab(this.imageTabButton, this.textTabButton);
+        });
+
+        this.textTabButton.addEventListener('click', () => {
+            this.imagePostsContainer.style.display = 'none';
+            this.textPostsContainer.style.display = 'block';
+            this.activateTab(this.textTabButton, this.imageTabButton);
+        });
+    }
+
+    fetchAndDisplayPosts(data) {
+        const postType = data;
+        fetch(data)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(item => {
+                    const postElement = document.createElement('div');
+                    postElement.classList.add('blog-post');
+
+                    const title = document.createElement('h2');
+                    title.textContent = item.title;
+                    postElement.appendChild(title);
+                    
+                    if (postType === 'texts_data.json') {
+                        this.addTextPost(postElement, item);
+                    } else if (postType === 'images_data.json') {
+                        this.addImagePost(postElement, item);
+                    }
+                });
+            })
+            .catch(error => console.error('Error fetching data:', error));
+        
+        this.activateDefaultTab();
+    }
+
+    addTextPost(container, item) {
+        const filePath = `texts/${item.file}`;
+        fetch(filePath)
+            .then(response => response.text())
+            .then(text => {
+                const paragraphs = text.split('\n');
+                paragraphs.forEach(paragraph => {
+                    const content = document.createElement('p');
+                    content.textContent = paragraph;
+                    container.appendChild(content);
+                });
+
+                this.textPostsContainer.appendChild(container);
+            })
+            .catch(error => console.error('Error fetching text post:', error));
+    }
+
+    addImagePost(container, item) {
+        const imagePath = `images/${item.image}`;
+        const image = document.createElement('img');
+        image.src = imagePath;
+        container.appendChild(image);
+
+        if (item.subtitle) {
+            const subtitle = document.createElement('p');
+            subtitle.classList.add('image-subtitle');
+            subtitle.textContent = item.subtitle;
+            container.appendChild(subtitle);
+        }
+
+        this.imagePostsContainer.appendChild(container);
+    }
+
+    activateDefaultTab() {
+        this.textTabButton.classList.add('active');
+        this.imageTabButton.classList.remove('active');
+        this.imagePostsContainer.style.display = 'none';
+        this.textPostsContainer.style.display = 'block';
+    }
+
+    activateTab(activeTab, inactiveTab) {
+        activeTab.classList.add('active');
+        inactiveTab.classList.remove('active');
+    }
+}
+
+const blog = new Blog();
+blog.init();
